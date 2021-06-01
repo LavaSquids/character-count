b2baaea4cbdbc1bc19517d812c21c5b9cc77acaf
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2021-06-01 05:21:44 +0100

    Add character count to editing messages

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { Plugin } from '@vizality/entities';
 import { patch, unpatchAll } from '@vizality/patcher';
 import { getModule } from '@vizality/webpack';
 
+const { getCurrentUser } = getModule('getCurrentUser');
+const { MAX_MESSAGE_LENGTH, MAX_MESSAGE_LENGTH_PREMIUM } = getModule('MAX_MESSAGE_LENGTH');
+
 export default class extends Plugin {
   start () {
     this.injectStyles('./style.css');
@@ -12,6 +15,14 @@ export default class extends Plugin {
   patch () {
     patch(getModule(m => m?.default?.displayName === 'SlateCharacterCount'), 'default', (args, res) => {
       return <div className={`${getModule('characterCount', 'upsell').characterCount} characterCount`}>{`${args[0].currentLength}/${args[0].maxCharacterCount}`}</div>;
+    });
+
+    patch(getModule(m => m?.displayName === 'MessageEditor').prototype, 'render', (args, res) => {
+      const { children } = res.props;
+      const MaxMessageLength = getCurrentUser().premiumType === 2 ? MAX_MESSAGE_LENGTH_PREMIUM : MAX_MESSAGE_LENGTH;
+
+      children.push(<div className={`${getModule('characterCount', 'upsell').characterCount} editCharacterCount`}>{`${children[0].props.textValue.length}/${MaxMessageLength}`}</div>);
+      return res;
     });
   }
 
