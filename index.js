8dac96576a3d830e82a19f8938e0f045d7d74f4e
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2021-09-25 17:38:20 +0100

    Improve counter

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Plugin } from '@vizality/entities';
-import { patch, unpatchAll } from '@vizality/patcher';
+import { patch } from '@vizality/patcher';
+const { user, constants: { Constants } } = require('@vizality/discord');
 import { getModule } from '@vizality/webpack';
 
-const { getCurrentUser } = getModule('getCurrentUser');
-const { MAX_MESSAGE_LENGTH, MAX_MESSAGE_LENGTH_PREMIUM } = getModule('MAX_MESSAGE_LENGTH');
+const { characterCount, error, flairContainer } = getModule('characterCount', 'upsell');
 
 export default class extends Plugin {
   start () {
@@ -13,20 +13,25 @@ export default class extends Plugin {
   }
 
   patch () {
-    patch(getModule(m => m?.default?.displayName === 'SlateCharacterCount'), 'default', (args, res) => {
-      return <div className={`${getModule('characterCount', 'upsell').characterCount} characterCount`}>{`${args[0].currentLength}/${args[0].maxCharacterCount}`}</div>;
+    patch(getModule(m => m.default?.displayName === 'SlateCharacterCount'), 'default', (args, res) => {
+      const { currentLength, maxCharacterCount } = args[0];
+
+      const className = currentLength > maxCharacterCount ? `${characterCount} ${error} CC-Container` : `${characterCount} CC-Container`;
+
+      return <div className={className}><div className={`${flairContainer} CC-Count`}>{`${currentLength}/${maxCharacterCount}`}</div></div>;
     });
 
-    patch(getModule(m => m?.displayName === 'MessageEditor').prototype, 'render', (args, res) => {
+    patch(getModule(m => m.displayName === 'MessageEditor').prototype, 'render', (args, res) => {
       const { children } = res.props;
-      const MaxMessageLength = getCurrentUser().premiumType === 2 ? MAX_MESSAGE_LENGTH_PREMIUM : MAX_MESSAGE_LENGTH;
 
-      children.push(<div className={`${getModule('characterCount', 'upsell').characterCount} editCharacterCount`}>{`${children[0].props.textValue.length}/${MaxMessageLength}`}</div>);
+      const CurrentLength = children[0].props.textValue.length;
+      const MaxMessageLength = user.getCurrentUser().premiumType === 2 ? Constants.MAX_MESSAGE_LENGTH_PREMIUM : Constants.MAX_MESSAGE_LENGTH;
+
+      const className = CurrentLength > MaxMessageLength ? `${characterCount} ${error} CC-EditCount` : `${characterCount} CC-EditCount`;
+
+      children.push(<div className={className}><div className={flairContainer}>{`${CurrentLength}/${MaxMessageLength}`}</div></div>);
+
       return res;
     });
-  }
-
-  stop () {
-    unpatchAll();
   }
 }
